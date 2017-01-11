b6bfd2db6fdd29f9c3a44af50b30d7cd95ee8433
Lee Pham <lee-pham@users.noreply.github.com>
2017-01-11 13:38:18 -0600

    Update light_accessory.js

--- accessories/light_accessory.js
+++ accessories/light_accessory.js
@@ -6,42 +6,42 @@ var PythonShell = require('python-shell');
 var pyshell = new PythonShell('bright.py');
 
 var LightController = {
-  name: "SenseHat", //name of accessory
-  pincode: "000-00-000",
-  username: "FA:7C:ED:5A:1A:1A", // MAC like address used by HomeKit to differentiate accessories. 
-  manufacturer: "The Raspberry Pi Foundation", //manufacturer (optional)
-  model: "v1.0", //model (optional)
-  serialNumber: "A12S345KGB", //serial number (optional)
-
-  power: false, //curent power 
-  
-  brightness: 100, //current brightness
-
-  outputLogs: true, //output logs
-
-  setPower: function(status) { //set power of accessory
+name: "SenseHat", //name of accessory
+pincode: "000-00-000",
+username: "FA:7C:ED:5A:1A:1A", // MAC like address used by HomeKit to differentiate accessories.
+manufacturer: "The Raspberry Pi Foundation", //manufacturer (optional)
+model: "v1.0", //model (optional)
+serialNumber: "A12S345KGB", //serial number (optional)
+    
+power: false, //curent power
+    
+brightness: 100, //current brightness
+    
+outputLogs: true, //output logs
+    
+setPower: function(status) { //set power of accessory
     if(this.outputLogs) console.log("Turning the '%s' %s", this.name, status ? "on" : "off");
     this.power = status;
-  },
-
-  getPower: function() { //get power of accessory
+},
+    
+getPower: function() { //get power of accessory
     if(this.outputLogs) console.log("'%s' is %s.", this.name, this.power ? "on" : "off");
     return this.power ? true : false;
-  },
-
-  setBrightness: function(brightness) { //set brightness
+},
+    
+setBrightness: function(brightness) { //set brightness
     if(this.outputLogs) console.log("Setting '%s' brightness to %s", this.name, brightness);
     this.brightness = brightness;
-  },
-
-  getBrightness: function() { //get brightness
+},
+    
+getBrightness: function() { //get brightness
     if(this.outputLogs) console.log("'%s' brightness is %s", this.name, this.brightness);
     return this.brightness;
-  },
-
-  identify: function() { //identify the accessory
+},
+    
+identify: function() { //identify the accessory
     if(this.outputLogs) console.log("Identify the '%s'", this.name);
-  }
+}
 }
 
 // Generate a consistent UUID for our light Accessory that will remain the same even when
@@ -58,48 +58,49 @@ lightAccessory.pincode = LightController.pincode;
 
 // set some basic properties (these values are arbitrary and setting them is optional)
 lightAccessory
-  .getService(Service.AccessoryInformation)
-    .setCharacteristic(Characteristic.Manufacturer, LightController.manufacturer)
-    .setCharacteristic(Characteristic.Model, LightController.model)
-    .setCharacteristic(Characteristic.SerialNumber, LightController.serialNumber);
+.getService(Service.AccessoryInformation)
+.setCharacteristic(Characteristic.Manufacturer, LightController.manufacturer)
+.setCharacteristic(Characteristic.Model, LightController.model)
+.setCharacteristic(Characteristic.SerialNumber, LightController.serialNumber);
 
 // listen for the "identify" event for this Accessory
 lightAccessory.on('identify', function(paired, callback) {
-  LightController.identify(); 
-  callback();
-});
+                  LightController.identify();
+                  callback();
+                  });
 
 // Add the actual Lightbulb Service and listen for change events from iOS.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
 lightAccessory
-  .addService(Service.Lightbulb, LightController.name) // services exposed to the user should have "names" like "Light" for this case
-  .getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
+.addService(Service.Lightbulb, LightController.name) // services exposed to the user should have "names" like "Light" for this case
+.getCharacteristic(Characteristic.On)
+.on('set', function(value, callback) {
     LightController.setPower(value);
-
+    
     // Our light is synchronous - this value has been successfully set
     // Invoke the callback when you finished processing the request
     // If it's going to take more than 1s to finish the request, try to invoke the callback
     // after getting the request instead of after finishing it. This avoids blocking other
     // requests from HomeKit.
     if (value) {
-      	PythonShell.run('light1.py', function (err) {
-			});
+    PythonShell.run('light1.py', function (err) {
+                    });
     } else {
-    	PythonShell.run('light0.py', function (err) {    				
-    	});
+    PythonShell.run('light0.py', function (err) {
+                    });
     }
     callback();
-  })
-  // We want to intercept requests for our current power state so we can query the hardware itself instead of
-  // allowing HAP-NodeJS to return the cached Characteristic.value.
-  .on('get', function(callback) {
+    console.log('does stuff get executed here?');
+    })
+// We want to intercept requests for our current power state so we can query the hardware itself instead of
+// allowing HAP-NodeJS to return the cached Characteristic.value.
+.on('get', function(callback) {
     callback(null, LightController.getPower());
-  });
+    });
 
 // To inform HomeKit about changes occurred outside of HomeKit (like user physically turn on the light)
 // Please use Characteristic.updateValue
-// 
+//
 // lightAccessory
 //   .getService(Service.Lightbulb)
 //   .getCharacteristic(Characteristic.On)
@@ -107,24 +108,24 @@ lightAccessory
 
 // also add an "optional" Characteristic for Brightness
 lightAccessory
-  .getService(Service.Lightbulb)
-  .addCharacteristic(Characteristic.Brightness)
-  .on('set', function(value, callback) {
+.getService(Service.Lightbulb)
+.addCharacteristic(Characteristic.Brightness)
+.on('set', function(value, callback) {
     LightController.setBrightness(value);
     pyshell.send(value);
-
+    
     pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    console.log(message);
-    });	
-    //pyshell.end();
-    pyshell.end(function (err) {
-	    if (err) throw err;
-	    console.log('finished');
-    });
+               // received a message sent from the Python script (a simple "print" statement)
+               console.log(message);
+               });
 
+    pyshell.end(function (err) {
+                if (err) throw err;
+                console.log('finished');
+                });
+    
     callback();
-  })
-  .on('get', function(callback) {
+    })
+.on('get', function(callback) {
     callback(null, LightController.getBrightness());
-  });
+    });
