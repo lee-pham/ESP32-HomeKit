dd89b18df2a256050511f879f5ad53d9d470a491
Lee Pham <lee-pham@users.noreply.github.com>
2017-01-11 21:44:26 -0600

    Create rgb.py

---
+++ rgb.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+import sys
+import colorsys
+
+sense = SenseHat()
+
+def normalize(H, S, B):
+  
+  
+  
+  
+while True:
+  line = sys.stdin.readline()
+
+  if line == '\n':
+    line = 0
+  else:   
+    line = int(line)
+  sense.clear(line, line, line)
