ec41649001ebc25ec315107e091d2338f108847d
Lee Pham <lee-pham@users.noreply.github.com>
2017-01-11 21:27:24 -0600

    Update sensehat_accessory.js

--- accessories/sensehat_accessory.js
+++ accessories/sensehat_accessory.js
@@ -1,115 +1,168 @@
-var PythonShell = require('python-shell');
-// HomeKit types required
-var types = require("./types.js")
-var exports = module.exports = {};
+var Accessory = require('../').Accessory;
+var Service = require('../').Service;
+var Characteristic = require('../').Characteristic;
+var uuid = require('../').uuid;
+var pyshell = new PythonShell('rgb.py');
 
-var execute = function(accessory,characteristic,value){ console.log("executed accessory: " + accessory + ", and characteristic: " + characteristic + ", with value: " +  value + "."); }
+var LightController = {
+  name: "Simple Light", //name of accessory
+  pincode: "000-00-000",
+  username: "FA:3C:ED:5A:4A:1A", // MAC like address used by HomeKit to differentiate accessories. 
+  manufacturer: "Raspberry Pi Foundation", //manufacturer (optional)
+  model: "Version 1.0", //model (optional)
+  serialNumber: "A12S345KGB", //serial number (optional)
 
-exports.accessory = {
-  displayName: "Light 1",
-  username: "1A:2B:3C:4D:5E:FF",
-  pincode: "031-45-154",
-  services: [{
-    sType: types.ACCESSORY_INFORMATION_STYPE, 
-    characteristics: [{
-    	cType: types.NAME_CTYPE, 
-    	onUpdate: null,
-    	perms: ["pr"],
-		format: "string",
-		initialValue: "RPi",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255    
-    },{
-    	cType: types.MANUFACTURER_CTYPE, 
-    	onUpdate: null,
-    	perms: ["pr"],
-		format: "string",
-		initialValue: "Oltica",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255    
-    },{
-    	cType: types.MODEL_CTYPE,
-    	onUpdate: null,
-    	perms: ["pr"],
-		format: "string",
-		initialValue: "Rev-1",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255    
-    },{
-    	cType: types.SERIAL_NUMBER_CTYPE, 
-    	onUpdate: null,
-    	perms: ["pr"],
-		format: "string",
-		initialValue: "A1S2NASF88EW",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255    
-    },{
-    	cType: types.IDENTIFY_CTYPE, 
-    	onUpdate: null,
-    	perms: ["pw"],
-		format: "bool",
-		initialValue: false,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Identify Accessory",
-		designedMaxLength: 1    
-    }]
-  },{
-    sType: types.LIGHTBULB_STYPE, 
-    characteristics: [{
-    	cType: types.NAME_CTYPE,
-    	onUpdate: null,
-    	perms: ["pr"],
-		format: "string",
-		initialValue: "Light 1 Light Service",
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Bla",
-		designedMaxLength: 255   
-    },{
-    	cType: types.POWER_STATE_CTYPE,
-    	onUpdate: function(value)
-	{ 
-    		console.log("Change:",value);
-    		if (value) {
-			PythonShell.run('light1.py', function (err) {
- 				console.log('On Success');
-			});
-    		} else {
-    			PythonShell.run('light0.py', function (err) {
-    				console.log("Off Success");
-    				
-    			});
-    		}
-    	},
-    	perms: ["pw","pr","ev"],
-		format: "bool",
-		initialValue: false,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Turn On the Light",
-		designedMaxLength: 1    
-    },{
-    	cType: types.BRIGHTNESS_CTYPE,
-    	onUpdate: function(value) { console.log("Change:",value); execute("Test Accessory 1", "Light - Brightness", value); },
-    	perms: ["pw","pr","ev"],
-		format: "int",
-		initialValue: 0,
-		supportEvents: false,
-		supportBonjour: false,
-		manfDescription: "Doesn’t actually adjust Brightness of Light",
-		designedMinValue: 0,
-		designedMaxValue: 100,
-		designedMinStep: 1,
-		unit: "%"
-    }]
-  }]
+  power: false, //curent power status
+  brightness: 100, //current brightness
+  hue: 0, //current hue
+  saturation: 0, //current saturation
+
+  outputLogs: false, //output logs
+
+  setPower: function(status) { //set power of accessory
+    if(this.outputLogs) console.log("Turning the '%s' %s", this.name, status ? "on" : "off");
+    this.power = status;
+  },
+
+  getPower: function() { //get power of accessory
+    if(this.outputLogs) console.log("'%s' is %s.", this.name, this.power ? "on" : "off");
+    return this.power ? true : false;
+  },
+
+  setBrightness: function(brightness) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' brightness to %s", this.name, brightness);
+    this.brightness = brightness;
+  },
+
+  getBrightness: function() { //get brightness
+    if(this.outputLogs) console.log("'%s' brightness is %s", this.name, this.brightness);
+    return this.brightness;
+  },
+
+  setSaturation: function(saturation) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' saturation to %s", this.name, saturation);
+    this.saturation = saturation;
+  },
+
+  getSaturation: function() { //get brightness
+    if(this.outputLogs) console.log("'%s' saturation is %s", this.name, this.saturation);
+    return this.saturation;
+  },
+
+  setHue: function(hue) { //set brightness
+    if(this.outputLogs) console.log("Setting '%s' hue to %s", this.name, hue);
+    this.hue = hue;
+  },
+
+  getHue: function() { //get hue
+    if(this.outputLogs) console.log("'%s' hue is %s", this.name, this.hue);
+    return this.hue;
+  },
+
+  identify: function() { //identify the accessory
+    if(this.outputLogs) console.log("Identify the '%s'", this.name);
+  }
 }
+
+// Generate a consistent UUID for our light Accessory that will remain the same even when
+// restarting our server. We use the `uuid.generate` helper function to create a deterministic
+// UUID based on an arbitrary "namespace" and the word "light".
+var lightUUID = uuid.generate('hap-nodejs:accessories:light' + LightController.name);
+
+// This is the Accessory that we'll return to HAP-NodeJS that represents our light.
+var lightAccessory = exports.accessory = new Accessory(LightController.name, lightUUID);
+
+// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
+lightAccessory.username = LightController.username;
+lightAccessory.pincode = LightController.pincode;
+
+// set some basic properties (these values are arbitrary and setting them is optional)
+lightAccessory
+  .getService(Service.AccessoryInformation)
+    .setCharacteristic(Characteristic.Manufacturer, LightController.manufacturer)
+    .setCharacteristic(Characteristic.Model, LightController.model)
+    .setCharacteristic(Characteristic.SerialNumber, LightController.serialNumber);
+
+// listen for the "identify" event for this Accessory
+lightAccessory.on('identify', function(paired, callback) {
+  LightController.identify();
+  callback();
+});
+
+// Add the actual Lightbulb Service and listen for change events from iOS.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+lightAccessory
+  .addService(Service.Lightbulb, LightController.name) // services exposed to the user should have "names" like "Light" for this case
+  .getCharacteristic(Characteristic.On)
+  .on('set', function(value, callback) {
+    LightController.setPower(value);
+
+    // Our light is synchronous - this value has been successfully set
+    // Invoke the callback when you finished processing the request
+    // If it's going to take more than 1s to finish the request, try to invoke the callback
+    // after getting the request instead of after finishing it. This avoids blocking other
+    // requests from HomeKit.
+    callback();
+    if (value) {
+	    pyshell.send([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+	    console.log([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+    } else {
+	    PythonShell.run('light0.py', function (err) {
+	    });
+    }
+  })
+  // We want to intercept requests for our current power state so we can query the hardware itself instead of
+  // allowing HAP-NodeJS to return the cached Characteristic.value.
+  .on('get', function(callback) {
+    callback(null, LightController.getPower());
+  });
+
+// To inform HomeKit about changes occurred outside of HomeKit (like user physically turn on the light)
+// Please use Characteristic.updateValue
+// 
+// lightAccessory
+//   .getService(Service.Lightbulb)
+//   .getCharacteristic(Characteristic.On)
+//   .updateValue(true);
+
+// also add an "optional" Characteristic for Brightness
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Brightness)
+  .on('set', function(value, callback) {
+    LightController.setBrightness(value);
+    callback();
+    pyshell.send([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+    console.log([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getBrightness());
+  });
+
+// also add an "optional" Characteristic for Saturation
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Saturation)
+  .on('set', function(value, callback) {
+    LightController.setSaturation(value);
+    callback();
+    pyshell.send([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+    console.log([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getSaturation());
+  });
+
+// also add an "optional" Characteristic for Hue
+lightAccessory
+  .getService(Service.Lightbulb)
+  .addCharacteristic(Characteristic.Hue)
+  .on('set', function(value, callback) {
+    LightController.setHue(value);
+    callback();
+    pyshell.send([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+    console.log([LightController.getHue(),LightController.getSaturation(),LightController.getBrightness()]);
+  })
+  .on('get', function(callback) {
+    callback(null, LightController.getHue());
+  });
