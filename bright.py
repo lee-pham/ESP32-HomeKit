b52d7084de67eb6155228fe126d8824ee2d2f03e
Lee Pham <lee-pham@users.noreply.github.com>
2017-01-11 21:05:22 -0600

    Update bright.py

--- bright.py
+++ bright.py
@@ -1,4 +1,13 @@
+from sense_hat import SenseHat
 import sys
+sense = SenseHat()
 
-for line in sys.stdin:
-  print(line)
+while True:
+        line = sys.stdin.readline()
+
+        if line == '\n':
+                line = 0
+        else:   
+                line = int(line)
+
+        sense.clear(line, line, line)
